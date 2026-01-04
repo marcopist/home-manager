9f0268141831839af45fa1679a40ac741d396893
marcopist <pistilli.mrc@gmail.com>
2026-01-04 09:38:29 +0000

    Initial home-manager configuration

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Home Manager configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      homeConfigurations."marcopist@nixos" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home.nix ];
+      };
+    };
+}
---
+++ home.nix
@@ -0,0 +1,46 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "marcopist";
+  home.homeDirectory = "/home/marcopist";
+  home.stateVersion = "25.11";
+
+  # Environment variables for Wayland/Hyprland
+  home.sessionVariables = {
+    WLR_NO_HARDWARE_CURSORS = "1";
+    NIXOS_OZONE_WL = "1";
+  };
+
+  # User packages (including fonts)
+  home.packages = with pkgs; [
+    # Development
+    neovim
+    nodejs
+    gh
+
+    # GUI applications
+    firefox
+
+    # Wayland/Hyprland tools
+    hyprland
+    waybar
+    kitty
+    wofi
+    dunst
+    grim
+    slurp
+    wl-clipboard
+    xdg-utils
+
+    # Fonts
+    nerd-fonts.fira-code
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.symbols-only
+  ];
+
+  # Enable fontconfig
+  fonts.fontconfig.enable = true;
+
+  # Enable Home Manager
+  programs.home-manager.enable = true;
+}
